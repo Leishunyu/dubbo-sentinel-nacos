1aa7c354e28bd4f6a9753099779d0940e85ef6ab
huaifeng <huaifeng@fanhaoyue.com>
2019-01-26 18:35:27 +0800

    dubbo spring boot zookeeper provider finished

--- dubbo-provider/src/main/java/com/huaifeng/dubbo/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/huaifeng/dubbo/provider/DubboProviderApplication.java
@@ -1,6 +1,5 @@
 package com.huaifeng.dubbo.provider;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
